e89785e71840d3fbac0b428719c6278ffc488faf
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2023-12-22 09:03:41 +0200

    Locked: use AuthBloc methods instead of pass them to constructor

--- lib/src/widgets/locked.dart
+++ lib/src/widgets/locked.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nonso/src/state/auth_bloc.dart';
 
 class Locked extends StatelessWidget {
   static const refreshString = "Refresh Account";
@@ -6,36 +8,28 @@ class Locked extends StatelessWidget {
       "Check your email to verify your email address";
   static const sendVerificationEmail = "Resend verification email";
   static const logout = "Log out";
-  final void Function() refreshAction;
-  final void Function() sendVerificationEmailAction;
-  final void Function() logoutAction;
   const Locked(
-      this.refreshAction, this.sendVerificationEmailAction, this.logoutAction,
       {Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    AuthBloc authBloc = BlocProvider.of<AuthBloc>(context);
     return Column(
       children: [
         const Text(verifyEmailAddress),
-        TextButton(
+        ElevatedButton(
+          onPressed: authBloc.updateUser,
           child: const Text(refreshString),
-          onPressed: () {
-            refreshAction();
-          },
         ),
-        TextButton(
+        ElevatedButton(
+          onPressed: authBloc.sendEmailToVerifyEmailAddress,
           child: const Text(sendVerificationEmail),
-          onPressed: () {
-            sendVerificationEmailAction();
-          },
         ),
-        TextButton(
-            child: const Text(logout),
-            onPressed: () {
-              logoutAction();
-            })
+        ElevatedButton(
+          onPressed: authBloc.signOut,
+          child: const Text(logout),
+        )
       ],
     );
   }
--- test/src/widgets/locked_test.dart
+++ test/src/widgets/locked_test.dart
@@ -1,24 +1,59 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:nonso/src/state/auth_bloc.dart';
+import 'package:nonso/src/state/auth_state.dart';
 import 'package:nonso/src/widgets/locked.dart';
 
+import '../state/auth_bloc_test.mocks.dart';
 import 'common_finders.dart';
-import 'email_test.mocks.dart';
 import 'skeleton_for_widget_testing.dart';
 
+class FakeAuthBloc extends Fake implements AuthBloc {
+  final AuthBloc _authBloc;
+
+  FakeAuthBloc(FirebaseAuth firebaseAuth) : _authBloc = AuthBloc(firebaseAuth);
+
+  @override
+  Stream<AuthState> get stream => _authBloc.stream;
+
+  @override
+  AuthState get state => _authBloc.state;
+
+  @override
+  Future<void> close() {
+    return _authBloc.close();
+  }
+}
+
 void main() {
-  final updateUserFunctionCall = MockToLogoutFunction();
-  final sendVerificationEmailFunctionCall = MockToLogoutFunction();
-  final signOutFunctionCall = MockToLogoutFunction();
+  const User? nullUser = null;
+  late StreamController<User?> streamController;
+  late BlocProvider widgetInSkeletonInBlocProvider;
+  late FirebaseAuth firebaseAuth;
+  late FakeAuthBloc authBloc;
   late Widget widgetInSkeleton;
+
   setUp(() {
-    widgetInSkeleton = createWidgetInASkeleton(Locked(updateUserFunctionCall,
-        sendVerificationEmailFunctionCall, signOutFunctionCall));
+    firebaseAuth = MockFirebaseAuth();
+    streamController = StreamController();
+    when(firebaseAuth.userChanges()).thenAnswer((_) => streamController.stream);
+    streamController.sink.add(nullUser);
+    authBloc = FakeAuthBloc(firebaseAuth);
+    widgetInSkeleton = createWidgetInASkeleton(const Locked());
+    widgetInSkeletonInBlocProvider = BlocProvider<AuthBloc>(
+      create: (context) => authBloc,
+      child: widgetInSkeleton,
+    );
   });
+
   testWidgets("Test the precense of the main widgets",
       (WidgetTester tester) async {
-    await tester.pumpWidget(widgetInSkeleton);
+    await tester.pumpWidget(widgetInSkeletonInBlocProvider);
     final lockedFinder = find.byType(Locked);
     expect(lockedFinder, findsOneWidget);
     expect(find.descendant(of: lockedFinder, matching: columnFinder),
@@ -27,54 +62,47 @@ void main() {
         find.descendant(
             of: columnFinder, matching: find.text(Locked.verifyEmailAddress)),
         findsOneWidget);
-    final refreshAccountTextButtonFinder = textButtonFinder.at(0);
+    final refreshAccountElevatedButtonFinder = elevatedButtonFinder.at(0);
+    final sendVerificationEmailElevatedButtonFinder =
+        elevatedButtonFinder.at(1);
+    final logoutElevatedButtonFinder = elevatedButtonFinder.at(2);
     expect(
         find.descendant(
-            of: columnFinder, matching: refreshAccountTextButtonFinder),
+            of: columnFinder, matching: refreshAccountElevatedButtonFinder),
         findsOneWidget);
     expect(
-        ((tester.widget(refreshAccountTextButtonFinder) as TextButton).child
-                as Text)
+        ((tester.widget(refreshAccountElevatedButtonFinder) as ElevatedButton)
+                .child as Text)
             .data,
         Locked.refreshString);
-    final sendVerificationEmailTextButtonFinder = textButtonFinder.at(1);
     expect(
         find.descendant(
-            of: columnFinder, matching: sendVerificationEmailTextButtonFinder),
+            of: columnFinder,
+            matching: sendVerificationEmailElevatedButtonFinder),
         findsOneWidget);
     expect(
-        ((tester.widget(sendVerificationEmailTextButtonFinder) as TextButton)
+        ((tester.widget(sendVerificationEmailElevatedButtonFinder)
+                    as ElevatedButton)
                 .child as Text)
             .data,
         Locked.sendVerificationEmail);
-    final logoutTextButtonFinder = textButtonFinder.at(2);
-    expect(find.descendant(of: columnFinder, matching: logoutTextButtonFinder),
+    expect(
+        find.descendant(of: columnFinder, matching: logoutElevatedButtonFinder),
         findsOneWidget);
     expect(
-        ((tester.widget(logoutTextButtonFinder) as TextButton).child as Text)
+        ((tester.widget(logoutElevatedButtonFinder) as ElevatedButton).child
+                as Text)
             .data,
         Locked.logout);
-  });
-  testWidgets("Test that clicking the refresh TextButton call its action",
-      (WidgetTester tester) async {
-    when(updateUserFunctionCall()).thenReturn(anything);
-    await tester.pumpWidget(widgetInSkeleton);
-    await tester.tap(textButtonFinder.at(0));
-    verify(updateUserFunctionCall()).called(1);
-  });
-  testWidgets(
-      "Test that clicking the sendVerificationEmail TextButton call its action",
-      (WidgetTester tester) async {
-    when(sendVerificationEmailFunctionCall()).thenReturn(anything);
-    await tester.pumpWidget(widgetInSkeleton);
-    await tester.tap(textButtonFinder.at(1));
-    verify(sendVerificationEmailFunctionCall()).called(1);
-  });
-  testWidgets("Test that clicking the logout TextButton calls its action",
-      (WidgetTester tester) async {
-    when(signOutFunctionCall()).thenReturn(anything);
-    await tester.pumpWidget(widgetInSkeleton);
-    await tester.tap(textButtonFinder.at(2));
-    verify(signOutFunctionCall()).called(1);
+    ElevatedButton refreshAccountElevatedButton =
+        tester.widget(refreshAccountElevatedButtonFinder);
+    ElevatedButton sendVerificationEmailElevatedButton =
+        tester.widget(sendVerificationEmailElevatedButtonFinder);
+    ElevatedButton logoutElevatedButton =
+        tester.widget(logoutElevatedButtonFinder);
+    expect(refreshAccountElevatedButton.onPressed, authBloc.updateUser);
+    expect(sendVerificationEmailElevatedButton.onPressed,
+        authBloc.sendEmailToVerifyEmailAddress);
+    expect(logoutElevatedButton.onPressed, authBloc.signOut);
   });
 }
