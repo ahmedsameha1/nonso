99ce3a46d1b9a9da381fe8e0df7e7376f0cdc784
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2023-12-23 13:11:13 +0200

    Export widgets

--- lib/nonso.dart
+++ lib/nonso.dart
@@ -1,3 +1,9 @@
 library nonso;
 
 export 'src/state/auth_bloc.dart' show AuthBloc;
+export 'src/widgets/auth_screen.dart' show AuthScreen;
+export 'src/widgets/sign_in_up.dart' show SignInUp;
+export 'src/widgets/email.dart' show Email;
+export 'src/widgets/password.dart' show Password;
+export 'src/widgets/register.dart' show Register;
+export 'src/widgets/locked.dart' show Locked;
