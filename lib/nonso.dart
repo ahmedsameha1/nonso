df84525036603d5327d820b6c55b595aa0777558
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-02-11 07:09:57 +0200

    export AuthState

--- lib/nonso.dart
+++ lib/nonso.dart
@@ -1,6 +1,7 @@
 library nonso;
 
 export 'src/state/auth_bloc.dart' show AuthBloc;
+export 'src/state/auth_state.dart' show AuthState;
 export 'src/widgets/auth_screen.dart' show AuthScreen;
 export 'src/widgets/auth_options.dart' show AuthOptions;
 export 'src/widgets/password.dart' show Password;
