da6c6bc4eb29e7a65acece29bbc4d23815d8a1a4
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-02-26 08:55:08 +0200

    Export ApplicationAuthState and AuthEvent to be referenced by other projects

--- lib/nonso.dart
+++ lib/nonso.dart
@@ -8,3 +8,6 @@ export 'src/widgets/password.dart' show Password;
 export 'src/widgets/register.dart' show Register;
 export 'src/widgets/locked.dart' show Locked;
 export 'l10n/app_localizations.dart' show AppLocalizations;
+export "src/state/value_classes/application_auth_state.dart"
+    show ApplicationAuthState;
+export 'src/state/auth_events.dart' show AuthEvent;
