d35a0f19a5b4761516384805e897567b5e10a21b
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-03-03 09:33:18 +0200

    Create a new Flutter app and update it to be an example of using this package

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Implement email and password authentication in your app by inserting ONE widget from this package in your widget tree.  
 It is based on [Firebase Authentication](https://firebase.google.com/docs/auth) and [Bloc](https://bloclibrary.dev/).
 
+<img alt="auth_options" src="https://raw.githubusercontent.com/ahmedsameha1/nonso/main/screenshots/auth_options.png" width="30%" hspace="10" vspace="5"><img alt="register" src="https://raw.githubusercontent.com/ahmedsameha1/nonso/main/screenshots/register.png" width="30%" hspace="10" vspace="5"><img alt="sign_in" src="https://raw.githubusercontent.com/ahmedsameha1/nonso/main/screenshots/sign_in.png" width="30%" hspace="5" vspace="10">
+
 ## Features
 
 - Register user accounts using email and password authentication.
---
+++ example/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:example/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nonso/nonso.dart' as nonso;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp(FirebaseAuth.instance));
+}
+
+class MyApp extends StatelessWidget {
+  final FirebaseAuth firebaseAuthInstance;
+  const MyApp(this.firebaseAuthInstance, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<nonso.AuthBloc>(
+      create: (context) => nonso.AuthBloc(firebaseAuthInstance),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        localizationsDelegates: [nonso.AppLocalizations.delegate],
+        supportedLocales: nonso.AppLocalizations.supportedLocales,
+        home: nonso.AuthScreen(
+          // This is your home screen
+          const MyHomePage(title: 'Flutter Demo Home Page'),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
